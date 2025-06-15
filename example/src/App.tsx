3a7c5e2f3f430daf002e072576c91ea2b9ffcef5
jCouth <craftcouth@gmail.com>
2025-06-15 15:39:24 -0300

    add nubank based design of initial screen

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,41 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-biometry-handler';
+import { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const result = multiply(3, 7);
+import {
+  isAvailable,
+  type SupportedBiometrics,
+} from 'react-native-biometry-handler';
+
+const status: Record<SupportedBiometrics, String> = {
+  STRONG: 'é do tipo "Forte"',
+  WEAK: 'é do tipo "Fraca"',
+  DEVICE_CREDENTIAL: 'é do tipo "Credenciais do Dispositivo"',
+  NONE: 'não está disponível',
+};
 
 export default function App() {
+  const [biometricStatus, setBiometricStatus] =
+    useState<SupportedBiometrics | null>(null);
+
+  const handleDevicePassword = () => {
+    Alert.alert('to do');
+  };
+
+  useEffect(() => {
+    setBiometricStatus(isAvailable());
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.logo}>react-native-biometry-handler</Text>
+      {biometricStatus ? (
+        <Text style={styles.feedback}>
+          A autenticação por biometria {status[biometricStatus]}!
+        </Text>
+      ) : null}
+      <TouchableOpacity style={styles.button} onPress={handleDevicePassword}>
+        <Text style={styles.buttonText}>Usar senha do celular</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,7 +43,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    gap: 16,
+    position: 'relative',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#9400d3',
+  },
+  logo: {
+    fontSize: 18,
+    color: 'white',
+  },
+  feedback: {
+    fontSize: 14,
+    color: 'white',
+  },
+  button: {
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 16,
+    padding: 16,
+    width: '100%',
+    borderRadius: 32,
+    backgroundColor: 'white',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
   },
 });
